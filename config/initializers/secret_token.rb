75b2e63d698a74ce13b0e05edc56be7f1be505f5
arsena21 <ericarsenault6@gmail.com>
2012-10-02 11:42:41 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollSpark::Application.config.secret_token = '328ebdcd6c883d1c788a5967ab33777582938dffe50e050a8ead7cddb332d4ac6b98901eb8254a110660f9cd4c8b498a0bedeb28df987fcbfcffc5a2f8b532a3'
