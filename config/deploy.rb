446e1489c6568e547f75966bf9edb6c6438aed1c
Ildar Manzhikov <manzhikov@gmail.com>
2012-11-17 02:11:18 +0800

    Added airbrake

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,3 +35,6 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+        require './config/boot'
+        require 'airbrake/capistrano'
---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,3 @@
+Airbrake.configure do |config|
+  config.api_key = 'd6d5c3b027bc0f455cef29859a1cc27e'
+end
