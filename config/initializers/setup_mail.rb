0f9452bb4a89726c04a8c5c0c9494ace9b3e07d8
raghavendra-prithvi <shiva@searchexperiences.com>
2013-01-18 21:43:07 +0530

    setup mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "generation315.com",
-  :user_name            => "ericarsenault6@gmail.com",
-  :password             => "",
+  :user_name            => "raghava.sangars@gmail.com",
+  :password             => "businessman",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
